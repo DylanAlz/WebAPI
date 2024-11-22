d61684c02bd0ae3c66e2ec6d8776bf5125edd9d4
Dylan <alzatedylan@gmail.com>
2024-11-21 23:09:26 -0500

    Creacion de la interface de state y el servicio sin logica aun

---
+++ DAL/SeederDB.cs
@@ -0,0 +1,70 @@
+﻿
+using WebAPI.DAL.Entities;
+
+namespace WebAPI.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBasecontext _context;
+
+        public SeederDB(DataBasecontext context)
+        {
+            _context = context;            
+        }
+
+        /*crearemos el metodo llamado seederasync(), este metodo es una especie de MAIN
+        Este metodo tendra la responsabilidad de prepoblar mi BD */
+        public async Task SeederAsync()
+        {
+            //este metodo creara la bd inmediatamente ponga en ejecucion la api
+            await _context.Database.EnsureCreatedAsync();
+
+            await PopulateCountriesAsync();
+        }
+
+        #region Private Methods
+        private async Task PopulateCountriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                //asi creo yo un pais con sus respectivos estados
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Antioquia"
+                        },
+
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cundinamarca"
+                        }
+                    }
+                });
+
+                //creando otro pais
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Alemania",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Hamburgo"
+                        }
+                    }
+                });
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Domain/Services/StateService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using WebAPI.DAL;
+using WebAPI.DAL.Entities;
+using WebAPI.Domain.Interfaces;
+
+namespace WebAPI.Domain.Services
+{
+    public class StateService : IStateService
+    {
+        private readonly DataBasecontext _context;
+        public StateService(DataBasecontext context)
+        {
+            _context = context;
+        }
+
+        public async Task<State> CreateStateAsync(Country country, State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<State> DeleteStateAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<State> EditStateAsync(Country country, State state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<State> GetStateByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<State>> GetStatesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
